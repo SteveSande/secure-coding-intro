e6c459400b9fbca4afb93627cd302df2a43dfd52
SteveSande <84928470+SteveSande@users.noreply.github.com>
2024-11-04 23:19:44 -0500

    Update sample.c

--- sample.c
+++ sample.c
@@ -12,6 +12,6 @@ int main(int argc, char** argv) {
         return -1;
     }
     char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
+    strncat(cmd, argv[1], BUFSIZE - strlen(cmd) - 1);
     system(cmd);
 }
