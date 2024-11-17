da0b0940cfe8d88c111d87c371f92cd21f79717a
SteveSande <84928470+SteveSande@users.noreply.github.com>
2024-11-17 16:54:03 -0500

    sim lab 3

--- sample.c
+++ sample.c
@@ -1,20 +1,8 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <stdlib.h>
     
-// This program prints the size of a specified file in bytes
-int main(int argc, char** argv) {
-    // Ensure that the user supplied exactly one command line argument
-    if (argc != 2) { 
-        fprintf(stderr, "Please provide the address of a file as an input.\n");
-        return -1;
-    }
-
-    struct stat fileStat;
-    if (stat(argv[1], &fileStat) != 0) {
-        fprintf(stderr, "Could not access file.\n");
-        return -1;
-    }
- 
-    printf("File size: %ld bytes\n", fileStat.st_size);
+int main(int argc, char **argv) {
+    char *items[] = {"boat", "car", "truck", "train"};
+    int index = atoi(argv[1]);
+    printf("You selected %s\n", items[index]);
 }
